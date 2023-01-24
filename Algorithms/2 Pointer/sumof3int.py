bd726c531dc32eb69e6015266e19ecd69bf1c083
ShriRachana <shrirachanar@gmail.com>
2023-01-23 21:32:05 -0800

    updated

---
+++ Algorithms/2 Pointer/sumof3int.py
@@ -0,0 +1,10 @@
+"""Given an array of integers, nums, and an integer value, target, determine if there are any three integers in nums whose sum equals the target. 
+Return TRUE if three such integers are found in the array. Otherwise, return FALSE.
+Constraints:
+3 <=num.length<=1000
+-10*3 <=nums[i] <=10*3
+-10*3 <=target <=10*3
+"""
+
+def fin_sum_of_three(nums, target):
+    
