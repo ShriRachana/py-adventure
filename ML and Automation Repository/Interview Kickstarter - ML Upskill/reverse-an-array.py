b460e8e2b8af2881b898b8a83baabfcf0e390ac7
ShriRachana <shrirachanar@gmail.com>
2023-09-22 06:51:21 -0700

    comments

--- ML and Automation Repository/Interview Kickstarter - ML Upskill/reverse-an-array.py
+++ ML and Automation Repository/Interview Kickstarter - ML Upskill/reverse-an-array.py
@@ -1,17 +1,16 @@
-'''
-Reverse An Array- Reverse a given list of numbers.
-Example One : {"nums": [50, 35, 78, 66, 17]}
-Output: [17, 66, 78, 35, 50]
+# Reverse An Array- Reverse a given list of numbers.
+# Example One : {"nums": [50, 35, 78, 66, 17]}
+# Output: [17, 66, 78, 35, 50]
 
-Example Two: {"nums": [50, 40, 30, 20]}
-Output: [20, 30, 40, 50]
+# Example Two: {"nums": [50, 40, 30, 20]}
+# Output: [20, 30, 40, 50]
 
-Notes: Modify the input array in-place and return the modified array.
-Constraints:
-1 <= size of the input array <= 106
-0 <= any element of the input array <= 106
-
-Pseudo Code Provided : 
+# Notes: Modify the input array in-place and return the modified array.
+# Constraints:
+# 1 <= size of the input array <= 106
+# 0 <= any element of the input array <= 106
+# ==========================================================================
+'''Pseudo Code Provided : 
 def reverse_array(nums):    
     Args:
      nums(list_int32)
@@ -26,6 +25,7 @@ lst.reverse()
 print("Using reverse() ", lst) 
 print("Using reversed() ", list(reversed(lst)))
 '''
+# ==========================================================================
 #Max Exe Time : 0.03 sec
 #Max Memory Used : 10.07 MB
 
