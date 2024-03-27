11a3af50d22cb7b350c106487bfbc9cb21208079
ShriRachana <shrirachanar@gmail.com>
2024-03-26 18:56:18 -0700

    push

---
+++ InterviewKickstarter/DSA/Notes.md
@@ -0,0 +1,4 @@
+## Foundations of Sorting Algorithms: 
+
+# For an input of 'n' numbers in no particular order, we need an algorithm that outputs these numbers in increasing and/or non-decreasing order. This problem can be extended to alphaets, characters, etc to order them in a lexicographical order. In the 1960's 70% of the prpblems computer engineeers dealt with was related to 
+
--- Language Proficiency/Python/Freecodecamp-Python/Giraffe/app.py
+++ Language Proficiency/Python/Freecodecamp-Python/Giraffe/app.py
@@ -1,3 +1,5 @@
+#Lesson 1 : Playing with Outputting data
+
 print("Hello World")
 
 #Drawing a shape wit Python 
@@ -18,3 +20,23 @@ print(character_name + " She was " + str(character_age) + " years old")
 character_name = "Shri Rachana"
 print(character_name + " She works as a SDE in a cool company")
 print("She does love crossfit")
+
+#Lesson 2 : Working with strings
+#New line is introduce with \n(backslash n)
+print("Giraffe\nAcademy")
+#Use backslash to help code print quotes or slashes 
+print("Giraffe\"Academy")
+phrase = "GIRAFFE Academy stored in a variable called phrase"
+print(phrase)
+#use concatenation to tie string together
+print(phrase + " is cool yo")
+
+#Lesson 3 : Function is nothing but a block of code that performs a SPECIFIC OPERATION for us. 
+#Example : Modify our string or get information from our string 
+#Example : Use function to do an action or operation on a string like make it all lower case. 
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.islower())
+#Example : Use multiple functions to peform multiple operations together. 
+print(phrase.upper().isupper())
